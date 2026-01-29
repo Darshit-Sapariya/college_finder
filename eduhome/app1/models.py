e4e8e381d3554d810a1930a35b3aac0d3e0fb3ea
Darshit-Sapariya <sapariyadarshit3596@gmail.com>
2026-01-29 20:10:06 +0530

    add database

--- eduhome/app1/models.py
+++ eduhome/app1/models.py
@@ -1,3 +1,9 @@
+from xml.parsers.expat import model
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    con_name = models.CharField(max_length=255)
+    con_email = models.EmailField()
+    con_subject = models.CharField(max_length=255)
+    con_message = models.TextField()
--- eduhome/app1/views.py
+++ eduhome/app1/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from app1.models import Contact
 # Create your views here.
 
 def index(request):
@@ -25,6 +27,18 @@ def blog(request):
     return render(request, 'blog.html')
 def blog_details(request):
     return render(request, 'blog-details.html')
+
+
 def contact(request):
+    if request.method == 'POST':
+        con_name = request.POST.get('con_name')
+        con_email = request.POST.get('con_email')
+        con_subject = request.POST.get('con_subject')
+        con_message = request.POST.get  ('con_message')
+        contact = Contact.objects.create(con_name=con_name, con_email=con_email, con_subject=con_subject, con_message=con_message)
+        return HttpResponse('Your message has been sent successfully. Thank you for contacting us.')
     return render(request, 'contact.html')
+
+
+                                             
 
